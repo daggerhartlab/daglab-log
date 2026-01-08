a00d5cc95bf5cc0742da1fa9803444318abdd39b
Jonathan Daggerhart <jonathan@daggerhartlab.com>
2026-01-08 14:16:37 -0500

    Bump version number

--- daglab-log.php
+++ daglab-log.php
@@ -6,7 +6,7 @@
  * GitHub Plugin URI: daggerhartlab/daglab-log
  * Primary Branch: main
  * Description: Logging plugin for WordPress.
- * Version: 1.0.4
+ * Version: 1.1.0
  * Author: Daggerhart Lab
  * Author URI: https://daggerhartlab.com
  * License: GPL-3.0+
